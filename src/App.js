271ef8830dec49bc9b1cae8a1df012a71129ff9e
BERIOT Maxime <mberiot@devoteam.com>
2023-04-27 16:26:35 +0200

    Test image

--- src/App.js
+++ src/App.js
@@ -25,31 +25,52 @@ const App = ({ signOut }) => {
     fetchNotes();
   }, []);
 
+  // async function fetchNotes() {
+  //   const apiData = await API.graphql({ query: listNotes });
+  //   const notesFromAPI = apiData.data.listNotes.items;
+  //   await Promise.all(
+  //     notesFromAPI.map(async (note) => {
+  //       if (note.image) {
+  //         const url = await Storage.get(note.name);
+  //         note.image = url;
+  //       }
+  //       return note;
+  //     })
+  //   );
+  //   setNotes(notesFromAPI);
+  // }
+
   async function fetchNotes() {
     const apiData = await API.graphql({ query: listNotes });
     const notesFromAPI = apiData.data.listNotes.items;
-    await Promise.all(
-      notesFromAPI.map(async (note) => {
-        if (note.image) {
-          const url = await Storage.get(note.name);
-          note.image = url;
-        }
-        return note;
-      })
-    );
     setNotes(notesFromAPI);
   }
+
+  // async function createNote(event) {
+  //   event.preventDefault();
+  //   const form = new FormData(event.target);
+  //   const image = form.get("image");
+  //   const data = {
+  //     name: form.get("name"),
+  //     description: form.get("description"),
+  //     image: image.name,
+  //   };
+  //   if (!!data.image) await Storage.put(data.name, image);
+  //   await API.graphql({
+  //     query: createNoteMutation,
+  //     variables: { input: data },
+  //   });
+  //   fetchNotes();
+  //   event.target.reset();
+  // }
 
   async function createNote(event) {
     event.preventDefault();
     const form = new FormData(event.target);
-    const image = form.get("image");
     const data = {
       name: form.get("name"),
       description: form.get("description"),
-      image: image.name,
     };
-    if (!!data.image) await Storage.put(data.name, image);
     await API.graphql({
       query: createNoteMutation,
       variables: { input: data },
